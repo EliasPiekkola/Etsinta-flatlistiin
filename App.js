821f06575a80cace385e261ee4a410139da8ef87
EliasPiekkola <112543659+EliasPiekkola@users.noreply.github.com>
2024-02-05 16:56:39 +0200

    valmis

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import {DATA} from './Data';
+import Row from './components/Row';
+import { useEffect, useState } from 'react';
+import Search from './components/Search';
+  /*
+  function renderItem({item}){
+    return (<Text>{item.lastname}</Text>)
+  }
+*/
 
+ /*
+  const renderItem = ({item}) => (
+    <Text>{item.lastname}</Text>
+  )
+  
+*/
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+const [items, setItems] =useState ([])
+
+
+useEffect (() => {
+    setItems(DATA)
+}, [])
+
+const executeSearch =(search) => {
+  const searchArray = DATA.filter((item) => item.lastname.startsWith(search))
+  setItems(searchArray)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.centeredContent}></View>
+        <View style={styles.searchContainer}></View>
+      <Search executeSearch={executeSearch} />
+      <FlatList
+      data={items}
+      renderItem = {({item}) => (
+        <Row person={item}/>
+      )}
+
+
+      ></FlatList>
+    </SafeAreaView>
+
+  );
+      }
+
+      const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          backgroundColor: '#fff',
+        },
+        centeredContent: {
+          flex: 1,
+          justifyContent: 'center', 
+          alignItems: 'center', 
+          width: '100%', 
+        },
+        searchContainer: {
+          marginBottom: 20, 
+        },
+        flatListCenteredContent: {
+          flexGrow: 1,
+          justifyContent: 'center', 
+        },
 });
---
+++ Data.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+export const DATA = [
+    {
+        id: '1',
+        firstname: 'Elias',
+        lastname: 'Piekkola'
+    },
+    {
+        id:'2',
+        firstname: 'Jouko',
+        lastname: 'Kaariala'
+    }
+]
